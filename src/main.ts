23e0fb6023be915f7a9292108a5d5e3e8c3ffed0
Tomás Jiménez <tjimenez999@gmail.com>
2020-07-21 11:31:12 -0400

    mejoras

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,38 @@
 import express, { Application, Request, Response } from 'express';
+import chalk from 'chalk';
+import morgan from 'morgan';
+import cors from 'cors';
 
-const app: Application = express();
 const LISTEN_PORT: number = 3000;
+const app: Application = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+const morganMiddleware = morgan(function (tokens, req, res) {
+    return [
+      chalk.green.bold("[" + tokens.method(req, res) + "]"),
+      chalk.red.bold(tokens.status(req, res)),
+      chalk.white("( route: " + tokens.url(req, res) + " )"),
+      chalk.yellow(tokens["response-time"](req, res) + " ms")
+    ].join(" ");
+  });
+
+app.use(morganMiddleware);
+
+
+
 
 app.get('/', function(req: Request, res: Response) {
     res.send('Servidor funcionando');
 });
 app.post('/dato',(req: Request, res: Response) => {
     console.log(req);
-    const body: any = req.body;
+    console.log(req.body);
     res.send({
         response: true,
-        body
+        body: req.body
     });
 });
 
